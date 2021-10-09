e582ebf784be08716944742e4895722d72e35b1d
spurgason <shanepurgason.98@gmail.com>
2021-10-09 14:44:48 -0400

    Notes finally post without having to delete db.json data to not get .toString not defined error

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path')
 const notes = require('../../db/db.json');
-
+const notesArray = [];
 
 router.get('/notes', (req, res) => {
     res.json(notes)
@@ -17,12 +17,11 @@ router.post('/notes', (req, res) => {
 
 function writeNewNote(body, notesArray) {
     const note = body;
-    const notesArray = [];
-
+    
     notesArray.push(note);
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2) 
+        JSON.stringify(notesArray, null, 2) 
     );
     return note;
 }
