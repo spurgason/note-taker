5f0a3954c006c8b4b4f0ec73a4fcc95f29bc82fc
spurgason <shanepurgason.98@gmail.com>
2021-10-09 13:02:51 -0400

    Trying to use Express.js's solution to making crud apps

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,30 +1,27 @@
 const router = require('express').Router();
 const fs = require('fs');
-const path = require('path')
-const notes = require('../../db/db.json');
-
 
 router.get('/notes', (req, res) => {
-    res.json(notes)
+    const note = getNote();
+    res.send(note);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const newNote = writeNewNote(req.body, notes);
-    res.json(newNote);
+    const existNotes = getNote();
+    const newNote = req.data;
+
+    existNotes.push(newNote);
+    saveNote(existNotes);
 });
 
+const saveNote = (data) => {
+    const stringifyData = JSON.stringify(data);
+    fs.writeFileSync('db/db.json', stringifyData);
+}
 
-function writeNewNote(body, notesArray) {
-    const note = body;
-    const notesArray = [];
-
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2) 
-    );
-    return note;
+const getNote = () => {
+    const noteData = fs.readFileSync('db/db.json');
+    return JSON.parse(noteData);
 }
 
 module.exports = router;
