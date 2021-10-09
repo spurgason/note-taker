fb383c2215dde69b6f11d7faff7fd58963e9ca09
spurgason <shanepurgason.98@gmail.com>
2021-10-09 16:01:37 -0400

    Changed Port to use Heroku or port 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const PORT = process.env || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(3001, () => {
-    console.log(`Server is on localhost:3001`);
+app.listen(PORT, () => {
+    console.log(`Server is on ${PORT}`);
 }); 
