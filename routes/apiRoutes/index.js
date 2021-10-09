3bd68abccecc9e9d44b47b290b4080f5edf174b9
spurgason <shanepurgason.98@gmail.com>
2021-10-09 15:16:42 -0400

    user can now delete item from notes, working on how to edit created notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -14,16 +14,38 @@ router.post('/notes', (req, res) => {
     res.json(newNote);
 });
 
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true)
+});
+
+router.patch('/notes/:id', (req, res) => {
+
+});
 
 function writeNewNote(body, notesArray) {
     const note = body;
-    
+
     notesArray.push(note);
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(notesArray, null, 2) 
+        JSON.stringify(notesArray, null, 2)
     );
     return note;
+}
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        const notes = notesArray[i];
+
+        if (notes.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, '../../db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+        }
+    }
 }
 
 module.exports = router;
